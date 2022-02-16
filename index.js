cf11bc199e633ed0f174d0188c485c2be87f41bb
Dylan Duario <53376196+DylanRoanDuario@users.noreply.github.com>
2022-02-16 18:10:11 +0400

    Regenerated token and used host env thing instead

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-require('dotenv').config();
-var token = process.env.token;
-
 const config = require("./config.json");
 
 client.on('ready',() => {
     console.log('Ready to suffer!');
 })
 
-client.login(token)
+client.login(process.env.TOKEN)
