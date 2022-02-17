f7cc0d35989babf3a06ac536e84a88a0e6cc49ba
Dylan Duario <53376196+DylanRoanDuario@users.noreply.github.com>
2022-02-17 12:15:51 +0400

    Added Heroku Postgress stuff

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { discordClient, Intents } = require('discord.js');
+const client = new discordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const config = require("./config.json");
 
@@ -8,3 +8,23 @@ client.on('ready',() => {
 })
 
 client.login(process.env.TOKEN)
+
+//Heroku Postgres stuff
+const { pgClient } = require('pg');
+
+const db = new pgClient({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+db.connect();
+
+db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  db.end();
+});
